4bcdff8e857ba5c9d182f4a514ec79d749f06dc3
Atiq <atiqul@mobiiworld.com>
2025-12-28 22:30:07 +0400

    Initial Commit

---
+++ Moodlytic/MoodlyticApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodlyticApp.swift
+//  Moodlytic
+//
+//  Created by Atiq Alam on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodlyticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
